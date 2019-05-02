6445eaa0763b0b4d3147ea9df632071676a67eb2
Paulo José <pauloup@gmail.com>
2019-05-02 05:03:12 -0300

    Fix to handle files with illegal characters (#53)
    * Fix to handle files with illegal characters
    
    * Add missing input file

--- tests/test_exif.py
+++ tests/test_exif.py
@@ -10,6 +10,10 @@ def test_exif_reads_valid_file():
     exif = Exif("input/exif.jpg")
     assert exif.data()['CreateDate'] == '2017:01:01 01:01:01'
 
+def test_exif_reads_files_with_illegal_characters():
+    exif = Exif("input/!#$%&'\"*+-.^_`|~:.jpg")
+    assert exif.data()['CreateDate'] == '2017:01:01 01:01:01'
+
 def test_exif_handles_exception(mocker):
     mocker.patch('subprocess.check_output', side_effect=CalledProcessError(2, 'cmd'))
     exif = Exif("not-existing.jpg")
