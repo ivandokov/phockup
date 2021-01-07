b8efc51711a5aec4a099202e073ed815fa550814
Ivan Dokov <ivan@jetspark.io>
2021-01-07 21:20:45 +0200

    Update snapcraft.yml to more simple setup

--- phockup.sh
+++ phockup.sh
@@ -6,4 +6,4 @@ export LANG=C
 
 export PERL5LIB="${PERL5LIB}:${SNAP}/usr/local/lib/x86_64-linux-gnu/perl/5.22.1:${SNAP}/usr/local/share/perl/5.22.1:${SNAP}/usr/lib/x86_64-linux-gnu/perl5/5.22:${SNAP}/usr/share/perl5:${SNAP}/usr/lib/x86_64-linux-gnu/perl/5.22:${SNAP}/usr/share/perl/5.22:${SNAP}/usr/local/lib/site_perl:${SNAP}/usr/lib/x86_64-linux-gnu/perl-base"
 
-exec "$SNAP/usr/bin/python3" "$SNAP/lib/phockup/phockup.py" "$@"
+exec "$SNAP/usr/bin/python3" "$SNAP/phockup.py" "$@"
