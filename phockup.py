379f6775154fbdac85de6088b0a68e0545347b0c
roykrikke <37672838+roykrikke@users.noreply.github.com>
2021-03-24 22:16:12 +0100

    Added quiet mode to not show output. (#103)

--- phockup.py
+++ phockup.py
@@ -144,6 +144,14 @@ To get all date fields available for a file, do:
     )
 
     parser.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="""Run without output
+        """,
+    )
+
+    parser.add_argument(
         "input_dir",
         metavar="INPUTDIR",
         help="Specify the source directory where your photos are located.",
@@ -167,6 +175,7 @@ To get all date fields available for a file, do:
         timestamp=args.timestamp,
         date_field=args.date_field,
         dry_run=args.dry_run,
+        quiet=args.quiet,
     )
 
 
