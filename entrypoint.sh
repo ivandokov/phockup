ad89ec59342e91f032e5f2438e3592c47b34ba07
Moritz <moritzf@gmail.com>
2022-03-13 13:04:27 +0100

    Add clean up lock on container restart
    if the container is killed in the middle of a phockup process and the lockfile persists, this will remove the lockfile once before the cron job is set up upon restarting the container.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -f /tmp/phockup.lockfile ]; then
+  rm /tmp/phockup.lockfile
+fi
+
 CRON_COMMAND="$CRON flock -n /tmp/phockup.lockfile phockup /mnt/input /mnt/output $OPTIONS"
 echo "$CRON_COMMAND" >> /etc/crontabs/root
 echo "cron job has been set up with command: $CRON_COMMAND"
