024b0e1ffdc355c00690e075e06848d3d8e4913a
Moritz <moritzf@gmail.com>
2022-07-04 15:03:58 +0200

    Add Cronjob and Environment Variables to Docker Image (#147)
    * Add Cronjob and environment variables to docker image
    
    * Prevent parallel execution of phockup
    
    * Add clean up lock on container restart
    
    if the container is killed in the middle of a phockup process and the lockfile persists, this will remove the lockfile once before the cron job is set up upon restarting the container.
    
    * Add previous behaviour to docker container
    
    The docker container continues to work as before if no CRON variable is set.
    If execution parameters ere defined through the OPTIONS variable, the container will use those in conujunction with the directories mounted to /mnt/input and /mnt/output. Both of those mount point are declared in the dockerfile.
    
    * Change ENV variables to only be considered for execution when running as cron job.
    
    * Update readme with description for running phocker as a cron job.
    
    * Fix naming in readme
    
    I mixed the wrong words there - its photos and back up not photos and docker.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,18 @@
 FROM python:3.9-alpine
 
+VOLUME /mnt/input
+VOLUME /mnt/output
+
+ENV CRON ""
+ENV OPTIONS ""
+
 COPY . /opt/phockup
+RUN chmod +x /opt/phockup/entrypoint.sh
 
 RUN apk --no-cache add exiftool \
     && pip install --no-cache-dir --use-feature=2020-resolver -r /opt/phockup/requirements.txt \
-    && ln -s /opt/phockup/phockup.py /usr/local/bin/phockup
+    && ln -s /opt/phockup/phockup.py /usr/local/bin/phockup \
+    && apk add bash \
+    && apk add flock
 
-ENTRYPOINT [ "phockup" ]
+ENTRYPOINT ["/opt/phockup/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# If the CRON variable is empty, phockup gets executed once as command line tool
+if [ -z "$CRON" ]; then
+  phockup $@
+
+# When CRON is not empty, phockup will run in a cron job until the container is stopped.
+else
+  if [ -f /tmp/phockup.lockfile ]; then
+    rm /tmp/phockup.lockfile
+  fi
+  
+  CRON_COMMAND="$CRON flock -n /tmp/phockup.lockfile phockup /mnt/input /mnt/output $OPTIONS"
+
+  echo "$CRON_COMMAND" >> /etc/crontabs/root
+  echo "cron job has been set up with command: $CRON_COMMAND"
+
+  crond -f -d 8
+fi
+
